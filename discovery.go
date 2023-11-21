e394dfd87f469c6fa30063baa3d038e23eb16729
杨益 <yangyi.cn.gz@gmail.com>
2023-11-21 13:14:04 +0800

    feat: 服务注册及发现

--- discovery.go
+++ discovery.go
@@ -1,6 +1,25 @@
 package nodehub
 
-import clientv3 "go.etcd.io/etcd/client/v3"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"path"
+	"time"
+
+	"go.etcd.io/etcd/api/v3/mvccpb"
+	clientv3 "go.etcd.io/etcd/client/v3"
+)
+
+// RegistryEvent 注册表事件
+type RegistryEvent int32
+
+const (
+	// PUT 新增或更新
+	PUT RegistryEvent = RegistryEvent(mvccpb.PUT)
+	// DELETE 删除
+	DELETE RegistryEvent = RegistryEvent(mvccpb.DELETE)
+)
 
 // NodeEntry 节点服务发现条目
 type NodeEntry struct {
@@ -10,11 +29,24 @@ type NodeEntry struct {
 	// 节点名称，仅用于显示
 	Name string `json:"name"`
 
+	// 节点状态
+	State string `json:"state"`
+
 	// grpc服务信息
 	GRPC GRPCEntry `json:"grpc"`
 
 	// git版本
 	GitVersion string `json:"git_version,omitempty"`
+}
+
+// Validate 验证条目是否合法
+func (e NodeEntry) Validate() error {
+	if e.ID == "" {
+		return errors.New("id is empty")
+	} else if e.Name == "" {
+		return errors.New("name is empty")
+	}
+	return nil
 }
 
 // GRPCEntry gRPC服务发现条目
@@ -50,11 +82,120 @@ type GRPCService struct {
 
 // Registry 服务注册表
 type Registry struct {
-	client    *clientv3.Client
-	keyPrefix string
+	client       *clientv3.Client
+	keyPrefix    string
+	eventHandler func(event RegistryEvent, entry NodeEntry)
+
+	leaseID clientv3.LeaseID
 }
 
-// Register 注册服务
-func (r *Registry) Register(entry NodeEntry) error {
+// NewRegistry 创建服务注册表
+func NewRegistry(client *clientv3.Client, opt ...func(*Registry)) (*Registry, error) {
+	r := &Registry{
+		client:    client,
+		keyPrefix: "/nodehub/node",
+		eventHandler: func(event RegistryEvent, entry NodeEntry) {
+			// do nothing
+		},
+	}
+
+	for _, fn := range opt {
+		fn(r)
+	}
+
+	if err := r.runKeeper(); err != nil {
+		return nil, fmt.Errorf("run keeper, %w", err)
+	}
+
+	go r.runWatcher()
+	return r, nil
+}
+
+// Put 注册服务
+func (r *Registry) Put(entry NodeEntry) error {
+	if r.leaseID == clientv3.NoLease {
+		return errors.New("lease not granted")
+	} else if err := entry.Validate(); err != nil {
+		return fmt.Errorf("validate entry, %w", err)
+	}
+
+	value, err := json.Marshal(entry)
+	if err != nil {
+		return fmt.Errorf("marshal entry, %w", err)
+	}
+
+	key := path.Join(r.keyPrefix, entry.ID)
+	_, err = r.client.Put(r.client.Ctx(), key, string(value), clientv3.WithLease(r.leaseID))
+	return err
+}
+
+// 向etcd生成一个10秒过期的租约，每5秒续约一次
+// 租约过期后，etcd会自动删除对应的key
+func (r *Registry) runKeeper() error {
+	lease, err := r.client.Grant(r.client.Ctx(), 10)
+	if err != nil {
+		return fmt.Errorf("grant lease, %w", err)
+	}
+	r.leaseID = lease.ID
+
+	go func() {
+		for {
+			ch, err := r.client.KeepAlive(r.client.Ctx(), r.leaseID)
+			if err != nil {
+				// TODO: 打日志
+			} else {
+				for resp := range ch {
+					_ = resp
+				}
+			}
+
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
 	return nil
+}
+
+// 监听服务条目变更
+func (r *Registry) runWatcher() {
+	wCh := r.client.Watch(r.client.Ctx(), r.keyPrefix, clientv3.WithPrefix(), clientv3.WithPrevKV())
+
+	for wResp := range wCh {
+		for _, ev := range wResp.Events {
+			var (
+				entry NodeEntry
+				value []byte
+			)
+			switch ev.Type {
+			case mvccpb.PUT:
+				value = ev.Kv.Value
+			case mvccpb.DELETE:
+				value = ev.PrevKv.Value
+			default:
+				panic(fmt.Errorf("unknown event type: %v", ev.Type))
+			}
+
+			if err := json.Unmarshal(value, &entry); err != nil {
+				panic(fmt.Errorf("unmarshal entry, %w", err))
+			}
+			r.eventHandler(RegistryEvent(ev.Type), entry)
+		}
+	}
+
+	// watcher永远不该退出
+	panic(errors.New("watcher closed"))
+}
+
+// WithKeyPrefix 设置服务条目key前缀
+func WithKeyPrefix(prefix string) func(*Registry) {
+	return func(r *Registry) {
+		r.keyPrefix = prefix
+	}
+}
+
+// WithEventHandler 设置服务条目变更事件处理函数
+func WithEventHandler(fn func(event RegistryEvent, entry NodeEntry)) func(*Registry) {
+	return func(r *Registry) {
+		r.eventHandler = fn
+	}
 }
