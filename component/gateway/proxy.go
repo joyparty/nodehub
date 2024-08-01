44fa7b73cbe273770e8221ada393b1c4f68cfd13
yangyi <yangyi.cq@qq.com>
2024-08-01 13:36:42 +0800

    refactor(gateway): 撤销上一条修改

--- component/gateway/proxy.go
+++ component/gateway/proxy.go
@@ -325,7 +325,7 @@ func (p *Proxy) handleRequest(ctx context.Context, sess Session, req *nh.Request
 		err = fmt.Errorf("request interceptor, %w", err)
 		return
 	} else if !pass {
-		logger.Debug("deny by request interceptor", "session", sess, "req", req)
+		err = errors.New("request interceptor denied")
 		return
 	}
 
